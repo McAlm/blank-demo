575b2b774841f76e20eb098d7cc7877c859de98f
Stefan Wiese <stefan.wiese@camunda.com>
2024-08-09 09:50:47 +0200

    adjusted config to spring-boot-starter-camunda-sdk:8.5

--- src/main/java/com/camunda/demo/BlankDemoApplication.java
+++ src/main/java/com/camunda/demo/BlankDemoApplication.java
@@ -1,16 +1,30 @@
 package com.camunda.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
+import lombok.extern.java.Log;
 
 @SpringBootApplication
-@Deployment(resources = {"classpath:bpmn/doNothing.bpmn"})
-public class BlankDemoApplication {
+@Log
+@Deployment(resources = { "classpath:bpmn/doNothing.bpmn" })
+public class BlankDemoApplication implements CommandLineRunner {
 
-public static void main(String[] args) {
+	@Autowired
+	private ZeebeClient zeebe;
+
+	public static void main(String[] args) {
 		SpringApplication.run(BlankDemoApplication.class, args);
 	}
 
+	public void run(final String... args) {
+		log.info("start process instance...");
+		zeebe.newCreateInstanceCommand().bpmnProcessId("DoNothing").latestVersion().variable("someVar", 1).send()
+				.join();
+		log.info("process instance started...");
+	}
 }
--- src/main/java/com/camunda/demo/worker/DemoWorker.java
+++ src/main/java/com/camunda/demo/worker/DemoWorker.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.stereotype.Component;
 
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import io.camunda.zeebe.spring.client.annotation.Variable;
 import lombok.extern.java.Log;
 
 @Component
@@ -12,8 +13,8 @@ import lombok.extern.java.Log;
 public class DemoWorker {
 
     @JobWorker(type = "demoWorker")
-    public Map<String, Object> runDemoTask(){
-        log.info("execute demo worker ");
+    public Map<String, Object> runDemoTask(@Variable Integer someVar){
+        log.info("execute demo worker with value " + someVar );
         return Map.of("demoExecuted", true);
     }
 }
