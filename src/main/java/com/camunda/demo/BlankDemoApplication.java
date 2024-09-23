f9cea73f29abae5f3989bcbf7ec620abc46324aa
Stefan Wiese <stefan.wiese@camunda.com>
2024-09-23 13:25:53 +0200

    8.6-alpha5

--- src/main/java/com/camunda/demo/BlankDemoApplication.java
+++ src/main/java/com/camunda/demo/BlankDemoApplication.java
@@ -1,11 +1,17 @@
 package com.camunda.demo;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.search.filter.VariableValueFilter;
+import io.camunda.zeebe.client.api.search.response.ProcessInstance;
+import io.camunda.zeebe.client.api.search.response.SearchQueryResponse;
+import io.camunda.zeebe.client.api.search.response.UserTask;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import lombok.extern.java.Log;
 
@@ -23,8 +29,60 @@ public class BlankDemoApplication implements CommandLineRunner {
 
 	public void run(final String... args) {
 		log.info("start process instance...");
-		zeebe.newCreateInstanceCommand().bpmnProcessId("DoNothing").latestVersion().variable("someVar", 1).send()
-				.join();
-		log.info("process instance started...");
+		for (Integer i : Arrays.asList(1,2,3,4,5,6,7,8,9,10)) {
+			
+			zeebe.newCreateInstanceCommand().bpmnProcessId("DoNothing").latestVersion().variable("someVar", i).send();
+					log.info("process instance started..." + i);
+		}
+
+		// SearchQueryResponse<UserTask> searchQueryResponse = zeebe.newUserTaskQuery()
+        //         .filter(userTaskFilter -> userTaskFilter.assignee("demo"))
+        //         .sort(s -> s.creationDate().asc())
+        //         .page(p -> p.limit(100))
+        //         .send().join();
+
+
+		// SearchQueryResponse<ProcessInstance> processInstances = zeebe.newProcessInstanceQuery()
+        //         .filter(f -> f.variable(new VariableValueFilter() {
+
+		// 			@Override
+		// 			public VariableValueFilter name(String value) {
+				
+		// 			}
+
+		// 			@Override
+		// 			public VariableValueFilter eq(Object value) {
+		// 				// TODO Auto-generated method stub
+		// 				throw new UnsupportedOperationException("Unimplemented method 'eq'");
+		// 			}
+
+		// 			@Override
+		// 			public VariableValueFilter gt(Object value) {
+		// 				// TODO Auto-generated method stub
+		// 				throw new UnsupportedOperationException("Unimplemented method 'gt'");
+		// 			}
+
+		// 			@Override
+		// 			public VariableValueFilter gte(Object value) {
+		// 				// TODO Auto-generated method stub
+		// 				throw new UnsupportedOperationException("Unimplemented method 'gte'");
+		// 			}
+
+		// 			@Override
+		// 			public VariableValueFilter lt(Object value) {
+		// 				// TODO Auto-generated method stub
+		// 				throw new UnsupportedOperationException("Unimplemented method 'lt'");
+		// 			}
+
+		// 			@Override
+		// 			public VariableValueFilter lte(Object value) {
+		// 				// TODO Auto-generated method stub
+		// 				throw new UnsupportedOperationException("Unimplemented method 'lte'");
+		// 			}
+					
+		// 		}))
+        //         .sort(s -> s.startDate().asc())
+        //         .page(p -> p.limit(100))
+        //         .send().join();
 	}
 }
